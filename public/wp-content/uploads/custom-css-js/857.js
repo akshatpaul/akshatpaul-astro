35a11e2fd85edef1567c4364fdca8827635ac840
Akshat Paul <akshatpaul@gmail.com>
2026-02-12 00:28:44 +0530

    Initial Astro site

---
+++ public/wp-content/uploads/custom-css-js/857.js
@@ -0,0 +1,11 @@
+<!-- start Simple Custom CSS and JS -->
+<script type="text/javascript">
+
+jQuery(document).ready(function($) {
+	$(document).on('click', '.elementor-popup-modal .dialog-close-button', function() {
+		setTimeout(function() {
+			elementorProFrontend.modules.popup.showPopup({ id: 'second_popup_show' });
+		}, 500); // Adjust the delay if needed
+	});
+});</script>
+<!-- end Simple Custom CSS and JS -->
